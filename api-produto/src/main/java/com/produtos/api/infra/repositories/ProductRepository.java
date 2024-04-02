fffbf4161932eee739ad7e6bf2b6cd0019b63b28
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-04-02 16:49:20 -0300

    feat: add repository product

---
+++ api-produto/src/main/java/com/produtos/api/infra/repositories/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.produtos.api.infra.repositories;
+
+import com.produtos.api.infra.models.Product;
+import com.produtos.api.infra.models.SubCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    public List<Product> findByNameProduct(String nameProduct);
+
+    public Product findBySku (int sku);
+
+    public List<Product> findBySubcategoria(SubCategory subCategory);
+
+}
