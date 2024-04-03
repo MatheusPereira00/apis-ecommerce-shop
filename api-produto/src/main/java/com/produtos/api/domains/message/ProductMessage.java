58d4a7590b4a6242ed2b7474e397ef4c6914b9a9
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-04-03 15:07:20 -0300

    feat: add messages

--- api-produto/src/main/java/com/produtos/api/domains/message/ProductMessage.java
+++ api-produto/src/main/java/com/produtos/api/domains/message/ProductMessage.java
@@ -3,4 +3,5 @@ package com.produtos.api.domains.message;
 public class ProductMessage {
 
     public static final String PRODUCT_NOT_FOUND_MESSAGE = "Desculpe, não foi possível encontrar um produto com este id Verifique e tente novamente";
+
 }
---
+++ api-produto/src/main/java/com/produtos/api/domains/message/SubCategoryMessage.java
@@ -0,0 +1,6 @@
+package com.produtos.api.domains.message;
+
+public class SubCategoryMessage {
+
+    public static final String SUBCATEGORY_NOT_FOUND_MESSAGE = "Desculpe, não foi possível encontrar uma subcategoria com esse id. Verifique e tente novamente.";
+}
