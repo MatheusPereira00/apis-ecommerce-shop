379d64fb55231c39e08f2fe965efde348c52c12d
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-04-09 14:08:30 -0300

    fix: add discount

--- api-desconto/src/main/java/com/desconto/products/controllers/DiscountProductController.java
+++ api-desconto/src/main/java/com/desconto/products/controllers/DiscountProductController.java
@@ -1,9 +1,15 @@
 package com.desconto.products.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/descount")
-public class DescountProductController {
+@RequestMapping("/discount/all")
+public class DiscountProductController {
+
+    @GetMapping
+    public String findAll(){
+        return "All Discount Products";
+    }
 }
